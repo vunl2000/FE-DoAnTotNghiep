fe8229e426227690b5a94aae33bfdb14ac7dd123
HuynhPT <danghuynh3009@gmail.com>
2022-05-24 16:48:29 +0700

    test

--- src/container/screens/ScreenCart.tsx
+++ src/container/screens/ScreenCart.tsx
@@ -1,14 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
 
 const ScreenCart = () => {
   return (
     <View>
+      {/* //ffffffff */}
       <Text>ScreenCart</Text>
     </View>
-  )
-}
+  );
+};
 
-export default ScreenCart
+export default ScreenCart;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
